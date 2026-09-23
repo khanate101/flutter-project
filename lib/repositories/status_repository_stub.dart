bb2a78f27bf430d6443faded12eccc2abc67084d
khanate101 <khanate.101@gmail.com>
2026-09-23 10:19:47 +0300

    Add Messenger and Business status sources

--- lib/repositories/status_repository_stub.dart
+++ lib/repositories/status_repository_stub.dart
@@ -5,15 +5,22 @@ class StatusRepository {
   final NativeStorageService nativeStorage = NativeStorageService();
 
   Future<bool> pickWhatsAppStatusFolder() => nativeStorage.pickStatusFolder();
-  Future<List<StatusItem>> scanAvailableMedia() async => _map(await nativeStorage.scanStatusFolder());
-  Future<bool> saveToGallery(StatusItem item) => nativeStorage.saveMedia(item.uri, item.title);
+
+  Future<List<StatusItem>> scanWhatsAppStatus(String type) async =>
+      _map(await nativeStorage.scanWhatsAppStatus(type));
+
+  Future<List<StatusItem>> scanAvailableMedia() async =>
+      _map(await nativeStorage.scanStatusFolder());
+
+  Future<bool> saveToGallery(StatusItem item) =>
+      nativeStorage.saveMedia(item.uri, item.title);
 
   List<StatusItem> _map(List<Map<String, dynamic>> items) => items.map((e) {
     final path = e['path'] as String;
     final mime = e['mime'] as String? ?? '';
     final isVideo = e['isVideo'] == true || mime.startsWith('video/');
     return StatusItem(
-      id: '$path|${e['size'] ?? 0}|${e['modified'] ?? 0}',
+      id: path + '|' + (e['size'] ?? 0).toString() + '|' + (e['modified'] ?? 0).toString(),
       uri: path,
       title: e['name'] as String? ?? path,
       type: isVideo ? StatusType.video : StatusType.image,
