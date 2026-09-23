b7bd910fa490fdbdb02dd0f46f0ff9bef86bca5c
khanate101 <khanate.101@gmail.com>
2026-09-23 11:10:28 +0300

    Add status folder picker stub

--- lib/core/services/native_storage_service_stub.dart
+++ lib/core/services/native_storage_service_stub.dart
@@ -1,6 +1,8 @@
 class NativeStorageService {
   Future<bool> pickStatusFolder() async => false;
   Future<List<Map<String, dynamic>>> scanStatusFolder() async => const [];
+  Future<bool> pickWhatsAppStatusFolder(String type) async => false;
+
   Future<List<Map<String, dynamic>>> scanWhatsAppStatus(String type) async => const [];
   Future<bool> saveMedia(String path, String title) async => false;
   Future<List<Map<String, dynamic>>> listSavedMedia() async => const [];
