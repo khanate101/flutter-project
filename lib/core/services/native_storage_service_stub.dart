a76abb7d48d5c8ec70f6ed7145dedce137b7e543
khanate101 <khanate.101@gmail.com>
2026-09-23 10:19:41 +0300

    Add Messenger and Business status sources

--- lib/core/services/native_storage_service_stub.dart
+++ lib/core/services/native_storage_service_stub.dart
@@ -1,6 +1,7 @@
 class NativeStorageService {
   Future<bool> pickStatusFolder() async => false;
   Future<List<Map<String, dynamic>>> scanStatusFolder() async => const [];
+  Future<List<Map<String, dynamic>>> scanWhatsAppStatus(String type) async => const [];
   Future<bool> saveMedia(String path, String title) async => false;
   Future<List<Map<String, dynamic>>> listSavedMedia() async => const [];
   Future<bool> deleteSavedMedia(String path) async => false;
